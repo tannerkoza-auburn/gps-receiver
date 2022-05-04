274ede34e5eccf7a1ecabcfb6aedce8ed60b1c9c
Tanner Koza <jtk0018@auburn.edu>
2022-05-03 19:36:12 -0500

    Uncomment lightspeed sv clock correction

--- classes/@gnssReceiver/p3D.m
+++ classes/@gnssReceiver/p3D.m
@@ -19,120 +19,126 @@ function out = p3D(obj, psr, svPos, svClockCorr)
 
 %% Initialization
 
-    % Handle Input Dimensions
-    [psr, svPos] = obj.dimHandle(psr, svPos);
+% Handle Input Dimensions
+[psr, svPos] = obj.dimHandle(psr, svPos);
 
-    % Define Number of Measurements
-    numMeas = length(psr);
+% Define Number of Measurements
+numMeas = length(psr);
 
-    % Initialize Shared Variables
-    uhat_x = 0;
-    uhat_y = 0;
-    uhat_z = 0;
-    y = 0;
-    G = 0;
+% Initialize Shared Variables
+uhat_x = 0;
+uhat_y = 0;
+uhat_z = 0;
+y = 0;
+G = 0;
 
-    % Initialize State Estimate Vector
-    est = [obj.initPos; obj.initClockBias];
-    
-    % Initialize Least Squares Iteration Count
-    itr = 0;
-    
-    % SV Clock Correction Unit Conversion (s to m)
-    C = physconst('LightSpeed');
-%     svClockCorr = svClockCorr * C;
+% Initialize State Estimate Vector
+est = [obj.initPos; obj.initClockBias];
+
+% Initialize Least Squares Iteration Count
+itr = 0;
+
+% SV Clock Correction Unit Conversion (s to m)
+C = physconst('LightSpeed');
+svClockCorr = svClockCorr * C;
 
 %% Estimation
 
-    % Least Squares & Newton-Raphson
-    while true
+% Least Squares & Newton-Raphson
+while true
 
-        unitVecs
-    
-        measVec
+    unitVecs
 
-        geomMatrix
+    measVec
 
-        dest = ( G' * G )^-1 * G' * y;
+    geomMatrix
 
-        est = est + dest;
+    dest = ( G' * G )^-1 * G' * y;
 
-        itr = itr + 1;
+    est = est + dest;
 
-        if  norm( dest(1:4) ) <= obj.lsconv
+    itr = itr + 1;
 
-            break
+    if  norm( dest(1:4) ) <= obj.lsconv
 
-        end
+        break
 
-    end 
+    end
 
-    % Covariance & DOP
-    DOP = ( G' * G )^-1;
-    P = obj.rcvrSigma * DOP;
+end
+
+% Covariance & DOP
+DOP = ( G' * G )^-1;
+P = obj.rcvrSigma * DOP;
 
 %% Solution Structure Population
 
-    % Populate Structure
-    out.pos = est(1:3);
-    out.clock_bias = est(4);
-    out.DOP = DOP;
-    out.P = P;
+% Populate Structure
+out.pos = est(1:3);
+out.clock_bias = est(4);
+out.DOP = DOP;
+out.P = P;
 
-    % Reinitialize Class Variables
-    % NOTE: Reinitializing initVel and initCloclDrift are unecessary
-    % because their final values are not invloved in Newton-Raphson.
-    obj.initPos = est(1:3);
-    obj.initClockBias = est(4);
+% Reinitialize Class Variables
+% NOTE: Reinitializing initVel and initCloclDrift are unecessary
+% because their final values are not invloved in Newton-Raphson.
+obj.initPos = est(1:3);
+obj.initClockBias = est(4);
 
 %% Nested Functions
 
     function unitVecs
-    
+
         % Initialization
         uhat_x = zeros(numMeas,1);
         uhat_y = zeros(numMeas,1);
         uhat_z = zeros(numMeas,1);
-    
+
         % Calculate Satellite Unit Vectors
         for i = 1:numMeas
-    
+
             r = sqrt( ( svPos(1,i) - est(1) )^2 ...
                 + ( svPos(2,i) - est(2) )^2 ...
                 + ( svPos(3,i) - est(3) )^2);
-    
+
             uhat_x(i) = ( svPos(1,i) - est(1) )/ r;
-    
+
             uhat_y(i) = ( svPos(2,i) - est(2) )/ r;
-    
+
             uhat_z(i) = ( svPos(3,i) - est(3) )/ r;
-    
+
         end
-    
+
     end
-    
+
     function measVec
-    
-        % Initialization 
+
+        % Initialization
         psrhat = zeros(numMeas,1);
-    
+
         % Measurement Vector Population
         for i = 1:numMeas
-    
-            psrhat(i) = sqrt( ( svPos(1,i) - est(1) )^2 ...
-                + ( svPos(2,i) - est(2) )^2 ...
-                + ( svPos(3,i) - est(3) )^2) + est(4) - svClockCorr(i);
-    
+
+            if svClockCorr(i) == 0
+                psrhat(i) = sqrt( ( svPos(1,i) - est(1) )^2 ...
+                    + ( svPos(2,i) - est(2) )^2 ...
+                    + ( svPos(3,i) - est(3) )^2) - svClockCorr(i);
+            else
+                psrhat(i) = sqrt( ( svPos(1,i) - est(1) )^2 ...
+                    + ( svPos(2,i) - est(2) )^2 ...
+                    + ( svPos(3,i) - est(3) )^2) + est(4) - svClockCorr(i);
+            end
+
         end
-    
+
         y = psr - psrhat;
-    
+
     end
-    
+
     function geomMatrix
-    
-    % Geometry Matrix Population
-     G = [-uhat_x -uhat_y -uhat_z ones(numMeas,1)];
+
+        % Geometry Matrix Population
+        G = [-uhat_x -uhat_y -uhat_z ones(numMeas,1)];
 
     end
 
